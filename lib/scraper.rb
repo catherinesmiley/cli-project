e24840498ad9394c46575148492b5bd966050af6
cesmiley <catherinesmiley@gmail.com>
2020-02-04 12:22:44 -0500

    fixed page variable

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,15 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 class Scraper 
 
     def self.scrape_page_categories
         binding.pry 
-        page = Nokogiri::HTML::open("https://www.goodreads.com/choiceawards/best-books-2019")
+        page = Nokogiri::HTML(open("https://www.goodreads.com/choiceawards/best-books-2019"))
         details = page.css("div.clearFix")
     end 
+
+    self.scrape_page_categories 
 
 end 
